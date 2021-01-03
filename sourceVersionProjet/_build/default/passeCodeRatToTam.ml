d513be1874f4304dbd1661897e06014ef60dad0e
AymenBenAbdallah <aymen.benabdallah9@live.fr>
2021-01-03 23:57:39 +0100

    début du projet après fin du tp

---
+++ sourceVersionProjet/_build/default/passeCodeRatToTam.ml
@@ -0,0 +1,158 @@
+(* Module de la passe de Typage *)
+module PasseCodeRatToTam : Passe.Passe with type t1 = Ast.AstPlacement.programme and type t2 = string =
+struct
+
+  open Type
+  open Tds
+  open Ast
+  open Exceptions
+  open AstType
+  open Code
+
+  type t1 = Ast.AstPlacement.programme
+  type t2 = string
+
+
+(* analyse_code_expression : AstType.expression -> String *)
+(* Paramètre e : l'expression à analyser *)
+(* Génère le code Tam de l'expression donnée *)
+(* Erreur si non cohérence des variables *)
+let rec analyse_code_expression e =
+  match e with
+    | True -> "LOADL 1\n"
+    | False -> "LOADL 0\n"
+    | AstType.Entier n ->
+      "LOADL " ^ (string_of_int n) ^ "\n"
+    | AstType.Binaire(b, e1, e2) ->
+      begin
+        let ne1 = analyse_code_expression e1 in
+        let ne2 = analyse_code_expression e2 in 
+        match b with
+        | PlusInt -> ne1 ^ ne2 ^ "SUBR IAdd \n"
+        | PlusRat -> ne1 ^ ne2 ^ "CALL (SB) RAdd \n"
+        | MultInt -> ne1 ^ ne2 ^ "SUBR IMul \n"
+        | MultRat -> ne1 ^ ne2 ^ "CALL (SB) RMul \n"
+        | EquInt ->  ne1 ^ ne2 ^ "SUBR IEq \n"
+        | EquBool -> ne1 ^ ne2 ^ "SUBR BEqu \n"
+        | Inf ->  ne1 ^ ne2 ^ "SUBR ILss \n"
+      end
+    | AstType.Rationnel(e1, e2) ->
+      begin
+        let ne1 = analyse_code_expression e1 in
+        let ne2 = analyse_code_expression e2 in 
+        ne1 ^ ne2
+      end
+    | AstType.Numerateur e ->
+      (analyse_code_expression e) ^ "POP (0) 1\n"
+    | AstType.Denominateur e ->
+      (analyse_code_expression e) ^ "POP (1) 1\n"
+    | AstType.Ident info_ast ->
+      begin
+        match (info_ast_to_info info_ast) with
+          | InfoConst (_, n) ->
+            "LOADL "^(string_of_int n)
+          | InfoVar (_, t, base, reg) ->
+            "LOAD (" ^ (string_of_int (getTaille t)) ^ ") " ^ (string_of_int base) ^ "[" ^ reg ^ "]\n"
+          | _ -> raise VariableIntrouvable
+      end
+    | AstType.AppelFonction(info, e) ->
+      begin
+        match (info_ast_to_info info) with
+          | InfoFun(n,_,_) ->
+                (List.fold_left (fun tt q -> tt ^ (analyse_code_expression q)) "" e ) ^ "CALL (SB) " ^ n ^ "\n" 
+          | _ -> raise FonctionIntrouvable
+          end
+
+(* analyse_code_instruction : AstType.instruction -> String *)
+(* Paramètre e : l'instruction à analyser *)
+(* Génère le code Tam de l'instruction donnée *)
+(* Erreur si non cohérence des variables *)
+let rec analyse_code_instruction i =
+  match i with
+  | AstType.Declaration (e, info) ->
+    begin
+      match (info_ast_to_info info) with
+        | InfoVar (_,t,base,reg) -> 
+          let ne = (analyse_code_expression e) in
+          "PUSH " ^ (string_of_int (getTaille t)) ^ "\n" ^
+          ne ^ 
+          "STORE (" ^ (string_of_int (getTaille t)) ^ ") " ^ (string_of_int base) ^ "[" ^ reg ^ "]\n"
+        | _ -> raise VariableIntrouvable
+    end
+  | AstType.Affectation (e,info) -> begin
+    match (info_ast_to_info info) with
+      | InfoVar (_,t,base,reg) -> 
+        let ne = analyse_code_expression e in
+        ne ^
+        "STORE (" ^ (string_of_int (getTaille t)) ^
+        ") " ^ (string_of_int base) ^ "[" ^ reg ^ "]\n" 
+      | _ -> raise VariableIntrouvable
+  end
+  | AstType.AffichageInt e ->
+    let ne = analyse_code_expression e in
+    ne ^ "SUBR IOut\n"
+  | AstType.AffichageRat e -> 
+    let ne = analyse_code_expression e in
+    ne ^ "CALL (SB) ROut\n"
+  | AstType.AffichageBool e -> 
+    let ne = analyse_code_expression e in
+    ne ^ "SUBR BOut\n"
+  | AstType.Conditionnelle (c,t,e) ->
+    begin
+      let nc = analyse_code_expression c in 
+      let nt = analyse_code_bloc t in
+      let ne = analyse_code_bloc e in
+      let labelElse = Code.getEtiquette() in
+      let labelFinElse = Code.getEtiquette() in
+      nc ^ "JUMPIF (0) " ^ labelElse ^ "\n" ^
+      nt ^ "\nJUMP " ^ labelFinElse ^ "\n" ^ 
+      labelElse ^ "\n" ^
+      ne ^ labelFinElse ^ "\n"
+    end
+  | AstType.TantQue (c,b) -> 
+    begin
+      let nc = (analyse_code_expression c) in 
+      let labelWhile = Code.getEtiquette() in
+      let labelEndWhile = Code.getEtiquette() in
+      labelWhile ^ "\n" ^ 
+      nc ^ "\nJUMPIF (0) " ^ labelEndWhile ^ "\n" ^
+      (analyse_code_bloc b) ^ "\nJUMP " ^ labelWhile ^ "\n" ^ 
+      labelEndWhile ^ "\n"
+    end
+  | AstType.Empty -> ""
+
+
+(* analyse_code_bloc : AstType.bloc -> String *)
+(* Paramètre li : le bloc (ou liste d'instructions) à analyser *)
+(* Génère le code Tam de le bloc donné *)
+(* Erreur si non cohérence des variables *)
+and analyse_code_bloc li =
+(List.fold_left (fun tt q -> tt ^ (analyse_code_instruction q)) "" li)
+
+(* analyser : AstType.ast -> string *)
+(* Paramètre : le programme à analyser *)
+(* Génère le code Tam de le bloc donné *)
+(* Erreur si non cohérence des variables *)
+let analyse_code_fonction (AstPlacement.Fonction(info_ast, _, li, r)) =
+  match (info_ast_to_info info_ast) with
+    | InfoFun (n, tr, tp) -> 
+      let taille_parametres = (List.fold_right (fun t tq -> (getTaille t)+tq) tp 0) in
+      let tailleVarLoc = (List.fold_right (fun t tq -> (AstType.taille_variables_declarees t)+tq) li 0) in
+      n ^ "\n" ^
+      (analyse_code_bloc li) ^
+      (analyse_code_expression r) ^
+      "POP (" ^ (string_of_int (getTaille tr)) ^ ") " ^ (string_of_int tailleVarLoc) ^ "\n" ^
+      "RETURN (" ^ (string_of_int (getTaille tr)) ^ ") " ^ (string_of_int taille_parametres) ^ "\n"
+    | _ -> raise FonctionIntrouvable
+
+
+(* analyser : AstPlacement.ast -> string *)
+(* Paramètre : le programme à analyser *)
+(* Génère le code Tam de le bloc donné *)
+(* Erreur si non cohérence des variables *)
+let analyser (AstPlacement.Programme (fonctions, prog)) =
+  let _ =  (List.fold_right (fun t tq -> (AstType.taille_variables_declarees t)+tq) prog 0) in
+    (getEntete ()) ^ (List.fold_left (fun tt q -> tt ^ (analyse_code_fonction q)) ""  fonctions) ^ "main\n" ^ (analyse_code_bloc prog) ^ "HALT\n"
+    (*"POP (0) " ^ (string_of_int taille_variables) ^ "\n" ^ "HALT\n"*)
+
+end
---
+++ sourceVersionProjet/passeTypeRat.ml
@@ -0,0 +1,221 @@
+(* Module de la passe de Typage *)
+module PasseTypeRat : Passe.Passe with type t1 = Ast.AstTds.programme and type t2 = Ast.AstType.programme =
+struct
+
+  open Type
+  open Tds
+  open Exceptions
+  open Ast
+  open AstType
+
+  type t1 = Ast.AstTds.programme
+  type t2 = Ast.AstType.programme
+
+
+(* analyse_type_expression : AstTds.expression -> AstType.expression *)
+(* Paramètre e : l'expression à analyser *)
+(* Vérifie la bonne cohérence des types utilisés et tranforme l'expression
+en une expression de type AstType.expression *)
+(* Erreur si cohérence entre les types utilisés *)
+let rec analyse_type_expression e = 
+  match e with
+    | AstTds.True -> (Type.Bool, AstType.True)
+    | AstTds.False -> (Type.Bool, False)
+    | AstTds.Entier n -> (Type.Int, Entier n)
+    | AstTds.Binaire (b, e1, e2) -> 
+    begin
+      let (te1, ne1) = analyse_type_expression e1 in
+      let (te2, ne2) = analyse_type_expression e2 in
+      match b with
+        | Plus -> 
+          begin
+            if ((Type.est_compatible te1 Type.Int) && (Type.est_compatible te2 Type.Int)) then 
+              (Type.Int, Binaire (PlusInt, ne1, ne2))
+            else if ((Type.est_compatible te1 Type.Rat) && (Type.est_compatible te2 Type.Rat)) then
+              (Type.Rat, Binaire (PlusRat, ne1, ne2))
+            else
+              raise (TypeBinaireInattendu(b, te1, te2))
+          end
+        | Mult -> 
+          begin
+            if ((Type.est_compatible te1 Type.Int) && ((Type.est_compatible te2 Type.Int))) then
+              (Type.Int, Binaire (MultInt, ne1, ne2))
+            else if ((Type.est_compatible te1 Type.Rat) && ((Type.est_compatible te2 Type.Rat))) then
+              (Type.Rat, Binaire (MultRat, ne1, ne2))
+            else 
+              raise (TypeBinaireInattendu(b,te1,te2))
+          end
+        | Equ -> 
+          begin
+            if ((Type.est_compatible te1 Type.Int) && ((Type.est_compatible te2 Type.Int))) then
+              (Type.Bool, Binaire (EquInt, ne1,ne2))
+            else if ((Type.est_compatible te1 Type.Bool) && ((Type.est_compatible te2 Type.Bool))) then
+              (Type.Bool, Binaire (EquBool,ne1,ne2))
+            else 
+              raise (TypeBinaireInattendu (b,te1,te2))
+          end
+        | Inf -> 
+          begin
+              if ((Type.est_compatible te1 Type.Int) && ((Type.est_compatible te2 Type.Int))) then
+                (Type.Bool, Binaire (Inf,ne1,ne2))
+              else 
+                raise (TypeBinaireInattendu (b,te1,te2))
+          end
+      end
+    | AstTds.Rationnel(e1, e2) ->
+      begin
+        let (te1,ne1) = analyse_type_expression e1 in
+        let (te2,ne2) = analyse_type_expression e2 in
+        match ((est_compatible te1 Type.Int),(est_compatible te2 Type.Int)) with
+          | (true,true) -> (Type.Rat, Rationnel (ne1,ne2))
+          | (true,false) -> raise (TypeInattendu (te2, Type.Int))
+          | (false,true) -> raise (TypeInattendu (te1, Type.Int))
+          | (false,false) -> raise (TypesParametresInattendus([te1; te2], [Type.Int; Type.Int]))
+      end
+    | AstTds.Numerateur e ->
+      begin
+        let (te,ne)=(analyse_type_expression e) in
+        if (est_compatible te Type.Rat) then 
+          (Type.Int, Numerateur ne)
+        else 
+          raise (TypeInattendu (te, Type.Rat))
+      end
+    | AstTds.Denominateur e ->
+      begin
+        let (te,ne)=(analyse_type_expression e) in
+        if (est_compatible te Type.Rat) then 
+          (Type.Int, Denominateur ne)
+        else 
+          raise (TypeInattendu (te, Type.Rat))
+      end
+    | AstTds.Ident info_ast ->
+      begin 
+        let info = info_ast_to_info info_ast in
+        match info with 
+          |InfoVar(_,t,_,_) -> (t, Ident(info_ast))
+          |InfoFun _ -> raise (MauvaiseUtilisationIdentifiant "")
+          |InfoConst (_,n) -> (Int, Entier n)
+      end
+    | AstTds.AppelFonction (info_ast, le) ->
+      begin
+        let i= (info_ast_to_info info_ast) in
+        match i with
+          | (InfoFun(_,tRetour,tParam)) -> 
+            begin
+              let nle = List.map (analyse_type_expression) le in
+              let (types, nlexp)=((List.map (fst) nle),(List.map (snd) nle)) in
+              if (est_compatible_list types tParam) then 
+                (tRetour, AppelFonction(info_ast, nlexp))
+              else raise (TypesParametresInattendus (tParam,types))
+            end
+          | _ -> failwith "error"
+      end
+
+
+(* analyse_type_instruction : AstTds.instruction -> tds -> AstType.instruction *)
+(* Paramètre i : l'instruction à analyser *)
+(* Vérifie la cohérence des types et tranforme l'instruction
+en une instruction de type AstTds.instruction *)
+(* Erreur si non cohérence des types *)
+let rec analyse_type_instruction i =
+  match i with
+  | AstTds.Declaration (t, e, info_ast) ->
+      begin
+        let (te, ne) = analyse_type_expression e in
+        if (est_compatible te t) then
+          begin
+            let _ = modifier_type_info te info_ast in
+            AstType.Declaration(ne,info_ast)
+          end
+        else
+          raise (TypeInattendu(te,t))
+      end
+      | AstTds.Affectation (e,info) ->
+      begin
+        let (te, ne) = analyse_type_expression e in 
+        begin
+          match info_ast_to_info info with
+            |InfoVar(_,t,_,_) -> 
+            begin
+              match (est_compatible te t) with
+              | true -> 
+                AstType.Affectation(ne, info);
+              | false -> 
+                raise (TypeInattendu(te,t))
+            end
+            | _ -> failwith("Error")
+        end
+
+      end
+        
+      | AstTds.Affichage e -> 
+      begin
+        let (te,ne)=(analyse_type_expression e) in
+          begin
+            if (est_compatible te Type.Int) then 
+              (AffichageInt ne)
+            else if (est_compatible te Type.Rat) then 
+              (AffichageRat ne)
+            else 
+              (AffichageBool ne)
+          end
+      end
+
+      | AstTds.Conditionnelle (c,t,e) -> 
+      begin
+        let (tc,nc) = analyse_type_expression c in
+        if (est_compatible tc Type.Bool) then 
+          let analyse_then = analyse_type_bloc t in
+          let analyse_else = analyse_type_bloc e in
+          Conditionnelle(nc,analyse_then,analyse_else)
+        else 
+          raise (TypeInattendu(tc,Type.Bool))
+      end
+
+      | AstTds.TantQue (c,b) -> 
+      begin
+        let (tc,nc) = analyse_type_expression c in
+        if (est_compatible tc Type.Bool) then 
+          let analyse_bloc = analyse_type_bloc b in
+          AstType.TantQue(nc, analyse_bloc)
+        else raise (TypeInattendu(tc, Type.Bool))
+      end
+      | AstTds.Empty -> Empty
+
+
+(* analyse_type_bloc : AstTds.bloc -> AstType.bloc *)
+(* Paramètre li : liste d'instructions à analyser *)
+(* Vérifie la cohérence des types et tranforme le bloc
+en un bloc de type AstType.bloc *)
+(* Erreur si non cohérence des types *)
+and analyse_type_bloc li =
+  List.map analyse_type_instruction li
+   
+
+(* analyse_type_fonction : AstTds.fonction -> AstType.fonction *)
+(* Paramètre : la fonction à analyser *)
+(* Vérifie la cohérence des types et tranforme la fonction
+en une fonction de type AstType.fonction *)
+(* Erreur si non cohérence des types *)
+let analyse_type_fonction (AstTds.Fonction(t,info_ast,info_param,li,retour))  = 
+  let ali = analyse_type_bloc li in
+  let (tr, nr) = analyse_type_expression retour in
+  let infoP = List.map (snd) info_param in
+  if (est_compatible tr t) then
+    Fonction(info_ast, infoP, ali, nr)
+  else
+    raise (TypeInattendu(tr,t))
+
+
+
+(* analyser : AstTds.ast -> AstType.ast *)
+(* Paramètre : le programme à analyser *)
+(* Vérifie la cohérence des types et tranforme le programme
+en un programme de type AstType.ast *)
+(* Erreur si non cohérence des types *)
+let analyser (AstTds.Programme (fonctions, prog)) =
+  let analyseF = List.map (analyse_type_fonction) fonctions in
+  let analyseB = analyse_type_bloc prog in
+  Programme (analyseF, analyseB)
+
+end
